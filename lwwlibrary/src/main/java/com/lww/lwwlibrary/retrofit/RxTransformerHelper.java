9307445336280295b947e868c049527b40fe78dc
liwenwei <296504966@qq.com>
2022-09-08 17:23:10 +0800

    修改Schedulers.io()为Schedulers.from()

--- lwwlibrary/src/main/java/com/lww/lwwlibrary/retrofit/RxTransformerHelper.java
+++ lwwlibrary/src/main/java/com/lww/lwwlibrary/retrofit/RxTransformerHelper.java
@@ -1,5 +1,9 @@
 package com.lww.lwwlibrary.retrofit;
 
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 import io.reactivex.Observable;
 import io.reactivex.ObservableSource;
 import io.reactivex.ObservableTransformer;
@@ -11,12 +15,14 @@ import io.reactivex.schedulers.Schedulers;
  * lww （2021-04-29 问题遗留）
  */
 public class RxTransformerHelper {
+    private static ThreadPoolExecutor executor = new ThreadPoolExecutor(20, 50,
+            60L, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
 
     public static <T> ObservableTransformer<T, T> observableIO2Main() {
         return new ObservableTransformer<T, T>() {
             @Override
             public ObservableSource<T> apply(Observable<T> upstream) {
-                return upstream.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
+                return upstream.subscribeOn(Schedulers.from(executor)).observeOn(AndroidSchedulers.mainThread());
             }
         };
     }
@@ -25,7 +31,7 @@ public class RxTransformerHelper {
         return new ObservableTransformer<T, T>() {
             @Override
             public ObservableSource<T> apply(Observable<T> upstream) {
-                return upstream.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
+                return upstream.subscribeOn(Schedulers.from(executor)).observeOn(AndroidSchedulers.mainThread());
             }
         };
     }
