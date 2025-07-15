350e8f2e080fc2265fe07e3a9859a27e6685536a
liwenwei <30338910+coolsili@users.noreply.github.com>
2025-07-15 11:20:38 +0800

    Update InterceptorHelper.java
    添加依赖

--- lwwlibrary/src/main/java/com/lww/lwwlibrary/retrofit/InterceptorHelper.java
+++ lwwlibrary/src/main/java/com/lww/lwwlibrary/retrofit/InterceptorHelper.java
@@ -7,8 +7,10 @@ import java.util.concurrent.TimeUnit;
 
 import okhttp3.CacheControl;
 import okhttp3.Interceptor;
+import okhttp3.MediaType;
 import okhttp3.Request;
 import okhttp3.Response;
+import okhttp3.RequestBody;
 import okhttp3.logging.HttpLoggingInterceptor;
 
 /**
